97cde698a021fc371dd6f0505848a4e72383d7fb
premchandAllu <premchandallu1999@gmail.com>
2020-11-09 20:09:05 +0530

    Added:Ability to check if employee is present or not

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,9 @@
-console.log("Welcome to Emp Wage Node JS");
+// UC_1 Checking if employee is present or absent
+const IS_ABSENT=0;
+let empCheck = Math.floor(Math.random()*10)%2;
+if(empCheck==0){
+    console.log("Employee is absent")
+}
+else{
+    console.log("Employee is present")
+}
