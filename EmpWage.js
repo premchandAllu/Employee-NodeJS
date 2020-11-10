f406c9ec86dad6ea583ff9bb0bd9f83184e83fff
premchandAllu <premchandallu1999@gmail.com>
2020-11-10 12:11:05 +0530

    Added:Method to calculate monthly wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,22 +1,25 @@
-//  UC_3 Refactor
+//  UC_4 Calculating monthly employee wage based on part time or full time work
 {
     const IS_PART_TIME = 0;
     const IS_FULL_TIME = 1;
     const PART_TIME_HOURS = 4;
     const FULL_TIME_HOURS = 8;
     const WAGE_PER_HOUR = 20;
+    const NO_OF_WORKING_DAYS = 20;
     function getWorkingHours(employeeCheck) {
         switch (employeeCheck) {
             case IS_PART_TIME:
                 return PART_TIME_HOURS;
             case IS_FULL_TIME:
                 return FULL_TIME_HOURS;
-            default:
-                return 0;
         }
     }
-    let employeeCheck = Math.floor(Math.random() * 10) % 3;
-    let employeeHours = getWorkingHours(employeeCheck);
+    let employeeHours = 0;
+    for (let day = 0; day < NO_OF_WORKING_DAYS; day++) {
+        let employeeCheck = Math.floor(Math.random() * 10) % 2;
+        employeeHours += getWorkingHours(employeeCheck);
+    }
     let employeeWage = employeeHours * WAGE_PER_HOUR;
+    console.log("Total Working Hours : " + employeeHours);
     console.log("Employee wage : $" + employeeWage);
 }
